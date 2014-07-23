45fd7397132f4ec64985760a507151ee3ce5f08f
kvnm <kevin@kevinmcconnaughay.com>
2014-07-22 20:39:01 -0400

    switch to CORS get request

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -16,7 +16,7 @@ angular.module('Warren.services', [])
         // Get from cache
         deferred.resolve(cache);
       } else {
-        $http.jsonp('//wcl.groupish.com/api/branches?callback=JSON_CALLBACK', { cache: true })
+        $http.get('//wcl.groupish.com/api/branches', { cache: true })
           .success(function (data) {
             // Store to cache
             branchesCache.put('allBranches', data);
@@ -33,24 +33,24 @@ angular.module('Warren.services', [])
   // Trigger the loading indicator
   return {
     show : function() { //code from the ionic framework doc
-       // Show the loading overlay and text
-       $rootScope.loading = $ionicLoading.show({
-         // The text to display in the loading indicator
-         content: 'Loading...',
-         // The animation to use
-         animation: 'fade-in',
-         // Will a dark overlay or backdrop cover the entire view
-         showBackdrop: true,
-         // The maximum width of the loading indicator
-         // Text will be wrapped if longer than maxWidth
-         maxWidth: 200,
-         // The delay in showing the indicator
-         showDelay: 500
-       });
+      // Show the loading overlay and text
+      $rootScope.loading = $ionicLoading.show({
+        // The text to display in the loading indicator
+        content: 'Loading...',
+        // The animation to use
+        animation: 'fade-in',
+        // Will a dark overlay or backdrop cover the entire view
+        showBackdrop: true,
+        // The maximum width of the loading indicator
+        // Text will be wrapped if longer than maxWidth
+        maxWidth: 200,
+        // The delay in showing the indicator
+        showDelay: 500
+      });
     },
     hide : function(){
       $rootScope.loading.hide();
     }
-  }
+  };
 })
 ;
